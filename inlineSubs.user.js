addce386c11c2ee62400533b130d4bb7a8c07d26
Yi Hein Chai <76833604+yiheinchai@users.noreply.github.com>
2022-02-13 12:24:35 +0000

    added draggable subtitle foundations and styling changes

--- inlineSubs.user.js
+++ inlineSubs.user.js
@@ -17,7 +17,15 @@
     document.querySelector(".transcript").click();
     document.querySelector(
       "#tooltips"
-    ).innerHTML = `<div class="inline-subs" style="width: 100vw; background-color: white; bottom: 50px; font-size: 22px; display: flex; flex-flow: row; justify-content: center; z-index: 3; position: fixed">Welcome to Yi Hein's Inline Subs</div>`;
+    ).innerHTML = `
+    <div style="width: ${width}px; bottom: 50px; display: flex; flex-flow: row; justify-content: center; z-index: 3; position: fixed"">
+                  <div
+                    class="inline-subs"
+                    style="background-color: rgba(8, 8, 8, 0.75); font-size: 22px; display: flex; flex-flow: row; justify-content: center; color: rgb(255, 255, 255); padding: 0.25rem; border-radius: 0.25rem"
+                  >Welcome to Yi Hein's Inline Subs
+                  </div>
+                </div>
+    `;
 
     function updateSubtitle() {
       const width = document
@@ -27,7 +35,8 @@
                 <div style="width: ${width}px; bottom: 50px; display: flex; flex-flow: row; justify-content: center; z-index: 3; position: fixed"">
                   <div
                     class="inline-subs"
-                    style="background-color: rgba(8, 8, 8, 0.75); font-size: 22px; display: flex; flex-flow: row; justify-content: center; color: rgb(255, 255, 255); padding: 0.25rem; border-radius: 0.25rem"
+                    draggable="true"
+                    style="background-color: rgba(8, 8, 8, 0.75); font-size: 22px; display: flex; flex-flow: row; justify-content: center; color: rgb(255, 255, 255); padding: 0.25rem 0.5rem 0.25rem 0.5rem; border-radius: 0.25rem"
                   >
                   </div>
                 </div>
@@ -44,6 +53,12 @@
       });
       document.querySelector(".transcript-panel").style.display = "none";
       document.querySelector(".sidebar").style.flex = "0 0";
+
+      // Enable drag and drop
+      const dragableElement = document.querySelector(".inline-subs")
+      // Add the ondragstart event listener
+      dragableElement.addEventListener("dragstart", dragstart_handler);
+
     } catch {
       document.querySelector(
         "#tooltips"
